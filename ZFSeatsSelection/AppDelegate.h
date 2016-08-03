60da308a315f9dbe1fcb320859ae0ba61ad9051b
ZFbaby <316917975@qq.com>
2016-08-03 09:22:04 +0800

    Initial Commit

---
+++ ZFSeatsSelection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZFSeatsSelection
+//
+//  Created by MAC_PRO on 16/8/3.
+//  Copyright © 2016年 MAC_PRO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZFSeatsSelection/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZFSeatsSelection
+//
+//  Created by MAC_PRO on 16/8/3.
+//  Copyright © 2016年 MAC_PRO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
